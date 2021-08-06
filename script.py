de644bbf6b680e520651a963417f21e301b87fd6
Ibrahima <ibracool99@gmail.com>
2021-08-06 11:38:00 +0100

    easygui

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 import os
+import easygui;
 
-path = "C:\\a"
+# path = "C:\\a"
 
 # entities = [["student", "admin"], ["don", "contribution"]]
-entities = [["h", "dfnd"],
-
-            ]
+from easygui import boolbox, multenterbox
 
 
 def replaceInFile(path, fromString, toString):
@@ -39,10 +38,10 @@ def getFiles(path):
 
 
 def replaceOsList(files, fromString, toString):
-    print("Replacing folders and files names from "+fromString+" to "+toString)
+    print("Replacing folders and files names from " + fromString + " to " + toString)
     for index, f in enumerate(files):
         if f[len(path):].__contains__(fromString):
-            new_file_name = path+f[len(path):].replace(fromString, toString)
+            new_file_name = path + f[len(path):].replace(fromString, toString)
             os.rename(f, new_file_name)
             files[index] = new_file_name
             print(new_file_name)
@@ -67,19 +66,52 @@ def run(fromString, toString):
 
 
 def cap(word):
-    if(len(word)):
+    if (len(word)):
         return word[0].upper() + word[1:]
     return ""
 
 
-for entity in entities:
-    fromString = entity[0]
-    toString = entity[1]
-    if(len(fromString)):
-        fromStrings = [fromString, fromString.lower(), cap(fromString)]
-        toStrings = [toString, toString.lower(), cap(toString)]
-        for i in range(0, 3):
-            run(fromStrings[i], toStrings[i])
-    else:
-        print("Cant replace to "+toString+" if there is no value ");
-        break;
+def gg():
+    for entity in entities:
+        fromString = entity[0]
+        toString = entity[1]
+        if (len(fromString)):
+            fromStrings = [fromString, fromString.lower(), cap(fromString)]
+            toStrings = [toString, toString.lower(), cap(toString)]
+            for i in range(0, 3):
+                run(fromStrings[i], toStrings[i])
+        else:
+            print("Cant replace to " + toString + " if there is no value ");
+            break;
+
+
+def setEntities():
+    msg = "Replace what ?"
+    title = "Rename"
+    fieldNames = ["From", "To"]
+    fieldValues = []  # we start with blanks for the values
+    fieldValues = multenterbox(msg, title, fieldNames)
+    print(fieldValues)
+    while 1:
+        if fieldValues == None: break
+        errmsg = ""
+        for i in range(len(fieldNames)):
+            if fieldValues[i].strip() == "":
+                errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+        if errmsg == "": break  # no problems found
+        fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
+    print("Reply was:", fieldValues);
+    return fieldValues;
+
+
+path = "";
+entities = [];
+message = "Chose folder"
+title = ""
+if boolbox(message, title, ["Import", "Cancel"]):
+    # path = easygui.diropenbox("Pick a folder", "xd", "C:\\Users\\Ibrahima\\git");
+    while boolbox("Add replacement", "Add replacement", ["Add", "Cancel"]):
+        entities.append(setEntities());
+    gg();
+else:
+    pass
