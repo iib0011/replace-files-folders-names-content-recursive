0c82cb3f9de5b2077244d01755707c0dfb5bd9f6
Ibrahima <ibracool99@gmail.com>
2021-08-06 13:51:56 +0100

    topdown false

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
 import os
 import easygui;
+import re;
 
 # path = "C:\\a"
 
 # entities = [["student", "admin"], ["don", "contribution"]]
 from easygui import boolbox, multenterbox
+
+
+def replace_folders(search, replace, path):
+    dirs = []
+    for r, d, f in os.walk(path, topdown=False):
+        for dir in d:
+            dirname = os.path.join(r, dir)
+            replaceFromAbsolute(dirname, search, replace)
+
+    print(dirs)
 
 
 def replaceInFile(path, fromString, toString):
@@ -40,16 +51,37 @@ def getFiles(path):
 def replaceOsList(files, fromString, toString):
     print("Replacing folders and files names from " + fromString + " to " + toString)
     for index, f in enumerate(files):
-        if f[len(path):].__contains__(fromString):
-            new_file_name = path + f[len(path):].replace(fromString, toString)
+        filename = f.split("\\")[-1];
+        print(f.split("\\")[-1]);
+        if filename.__contains__(fromString):
+            new_file_name = path[:len(filename)] + "\\" + filename.replace(fromString, toString)
             os.rename(f, new_file_name)
             files[index] = new_file_name
             print(new_file_name)
 
 
+def replaceFromAbsolute(f, fromString, toString):
+    filename = f.split("\\")[-1];
+    print(f.split("\\")[-1]);
+    if filename.__contains__(fromString):
+        new_file_name = path[:len(filename)] + "\\" + filename.replace(fromString, toString)
+        if (not os.path.exists(new_file_name)):
+            os.rename(f, new_file_name)
+            print(new_file_name)
+
+
 def updateDirectories(path, fromString, toString):
-    directories = getDirectories(path)
-    replaceOsList(directories, fromString, toString)
+    for f in os.listdir(path):
+        old = f
+        f = f.replace(" ", "_")
+        f = re.sub(r'[^a-zA-Z0-9-_]', '', f)
+        if old != f:
+            os.rename(old, "hd")
+            print("renamed " + old + " to " + f)
+        if os.path.isdir(f):
+            os.chdir(f)
+            updateDirectories(".")
+            os.chdir("..")
 
 
 def updateFiles(path, fromString, toString):
@@ -60,9 +92,9 @@ def updateFiles(path, fromString, toString):
 # r=root, d=directories, f =
 
 def run(fromString, toString):
-    updateDirectories(path, fromString, toString)
-    updateFiles(path, fromString, toString)
     replaceInFile(path, fromString, toString)
+    updateFiles(path, fromString, toString)
+    replace_folders(fromString, toString, path)
 
 
 def cap(word):
@@ -71,18 +103,19 @@ def cap(word):
     return ""
 
 
-def gg():
-    for entity in entities:
-        fromString = entity[0]
-        toString = entity[1]
-        if (len(fromString)):
-            fromStrings = [fromString, fromString.lower(), cap(fromString)]
-            toStrings = [toString, toString.lower(), cap(toString)]
-            for i in range(0, 3):
-                run(fromStrings[i], toStrings[i])
-        else:
-            print("Cant replace to " + toString + " if there is no value ");
-            break;
+def mapEntities(path, entities):
+    if (len(entities)):
+        for entity in entities:
+            fromString = entity[0];
+            toString = entity[1];
+            if (len(fromString)):
+                fromStrings = [fromString, fromString.lower(), cap(fromString)]
+                toStrings = [toString, toString.lower(), cap(toString)]
+                for i in range(0, 3):
+                    replace_folders(fromStrings[i], toStrings[i], path)
+            else:
+                print("Cant replace to " + toString + " if there is no value ");
+                break;
 
 
 def setEntities():
@@ -104,14 +137,25 @@ def setEntities():
     return fieldValues;
 
 
-path = "";
-entities = [];
+path = "C:\\a";
+entities = [["l", "jm"]];
 message = "Chose folder"
 title = ""
 if boolbox(message, title, ["Import", "Cancel"]):
-    # path = easygui.diropenbox("Pick a folder", "xd", "C:\\Users\\Ibrahima\\git");
-    while boolbox("Add replacement", "Add replacement", ["Add", "Cancel"]):
+    path = easygui.diropenbox("Pick a folder", "xd", "C:\\Users\\Ibrahima\\git");
+    while boolbox("Add replacement", "Add replacement?", ["Add", "No"]):
         entities.append(setEntities());
-    gg();
+    mapEntities(path, entities);
 else:
     pass
+
+"""
+Usage: python script.py search_string replace_string dir
+Eg. python batchreplace.py galleries productions /Sites/cjc/application/modules/productions/
+And it will search recursively in dir
+and replace search_string in contents
+and in filenames.
+Case-sensitive
+"""
+
+from sys import argv
